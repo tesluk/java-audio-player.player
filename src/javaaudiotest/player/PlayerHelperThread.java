0f4b2f1d0ac19b2686f646134d31d790ce2e5cf3
cy6erGn0m <cy6erGn0m@localhost>
2009-09-28 12:07:49 +0400

    Notificator runs more stable, oes not shakes CPU. Stop after pause now works.

--- src/javaaudiotest/player/PlayerHelperThread.java
+++ src/javaaudiotest/player/PlayerHelperThread.java
@@ -29,41 +29,54 @@ public class PlayerHelperThread extends Thread {
             int size;
             float rate;
             long time;
+            long now, lpa, timeToSleep;
+
             while( !dieRequested && !isStopped() ) {
-                sleep( 150 );
-                if( ( line = parent.getCurrentDataLine() ) != null ) {
-                    size = line.getBufferSize();
-                    available = line.available();
-                    time = line.getMicrosecondPosition();
+                now = System.currentTimeMillis();
+                lpa = parent.lastPlayerActivity;
+                timeToSleep = 400 - ( now - lpa );
+                if( timeToSleep < 200 )
+                    timeToSleep = 200;
 
-                    parent.currentPlayTimeMcsec = time;
+                sleep( timeToSleep );
 
-                    rate = ( (float) available ) / ( (float) size );
-                    synchronized( parent.osync ) {
+                if( parent.lastPlayerActivity == lpa ) {
+                    if( ( line = parent.getCurrentDataLine() ) != null ) {
+                        size = line.getBufferSize();
+                        available = line.available();
+
+                        rate = ( (float) available ) / ( (float) size );
                         final PlayerState state = parent.getState();
-                        if( rate > 0.9 ) {
-                            if( state == PlayerState.PLAYING && !parent.getPumpStream().isAllDownloaded() ) {
-                                System.out.println( "low speed detected.. lets pause" );
-                                parent.setState( PlayerState.PAUSED_BUFFERING );
-                                parent.osync.notifyAll();
-                                continue;
-                                //line.stop();   // !! NEVER STOP LINE to prevent player-playback-thread spinning on Linux
-                            }
-                        }
-                        if( state == PlayerState.PAUSED_BUFFERING ) {
-                            if( ( /*rate == 0. && */ ( ( parent.currentBufferedTimeMcsec - parent.currentPlayTimeMcsec ) > 10000000L ) ) || parent.getPumpStream().isAllDownloaded() ) {
-                                // NOTE: rate unchecked, because in this state player-playback-thread sleeping and can't fill device buffer
-                                System.out.println( "lets resume" );
-                                parent.osync.wait( 500 );
-                                parent.setState( PlayerState.PLAYING );
-                                parent.osync.notifyAll();
-                                parent.osync.wait( 500 );
-                                //line.start();
-                            }
-                        }
-                    }
+                        if( state == PlayerState.PAUSED_BUFFERING || rate > 0.9 ) {
+                            time = parent.getCurrentPosition();
 
+                            synchronized( parent.osync ) {
+                                if( rate > 0.9 ) {
+                                    if( state == PlayerState.PLAYING && !parent.getPumpStream().isAllDownloaded() ) {
+                                        System.out.println( "low speed detected.. lets pause" );
+                                        parent.setState( PlayerState.PAUSED_BUFFERING );
+                                        parent.osync.notifyAll();
+                                        continue;
+                                        //line.stop();   // !! NEVER STOP LINE to prevent player-playback-thread spinning on Linux
+                                    }
+                                }
+                                if( state == PlayerState.PAUSED_BUFFERING ) {
+                                    if( ( /*rate == 0. && */ ( ( parent.currentBufferedTimeMcsec - time ) > 10000000L ) ) || parent.getPumpStream().isAllDownloaded() ) {
+                                        // NOTE: rate unchecked, because in this state player-playback-thread sleeping and can't fill device buffer
+                                        System.out.println( "lets resume" );
+                                        parent.osync.wait( 500 );
+                                        parent.setState( PlayerState.PLAYING );
+                                        parent.osync.notifyAll();
+                                        parent.osync.wait( 500 );
+                                        //line.start();
+                                    }
+                                }
+                            }
+                        }
+
+                    }
                 }
+
             }
         } catch( InterruptedException ignore ) {
         } finally {
