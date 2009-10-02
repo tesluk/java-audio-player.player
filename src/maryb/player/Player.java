eb0379d0d314630c13988a1bcdc2a77367819c41
cy6erGn0m <cy6erGn0m@localhost>
2009-10-03 02:03:22 +0400

    Fixed regression: max time was 0ms after pause and then play Fixed issue: now player updates current bufferred time value at pause\stop Player thread extracted from player class

--- src/maryb/player/Player.java
+++ src/maryb/player/Player.java
@@ -2,32 +2,11 @@ package maryb.player;
 
 import maryb.player.io.SeekablePumpStream;
 import java.io.Closeable;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.InterruptedIOException;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import maryb.player.io.EmulateSlowInputStream;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
 import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.Line;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
-import javazoom.jl.decoder.Bitstream;
-import javazoom.jl.decoder.BitstreamException;
-import javazoom.jl.decoder.Decoder;
-import javazoom.jl.decoder.DecoderException;
-import javazoom.jl.decoder.Header;
-import javazoom.jl.decoder.SampleBuffer;
 
 /**
  *
@@ -46,15 +25,15 @@ public final class Player {
 
     /* package */ volatile long currentPlayTimeMcsec = 0;
 
-    private volatile long currentSeekPositionMcsec = 0;
+    /* package */ volatile long currentSeekPositionMcsec = 0;
 
-    private volatile long totalPlayTimeMcsec = 0;
+    /* package */ volatile long totalPlayTimeMcsec = 0;
 
     private PlayerEventListener listener;
 
     /* package */ volatile long lastPlayerActivity = 0;
 
-    private boolean endOfMediaReached = false;
+    /* package */ boolean endOfMediaReached = false;
 
     public float getCurrentVolume() {
         return currentVolume;
@@ -144,25 +123,11 @@ public final class Player {
 
     /* package */ final Object osync = new Object();
 
-    private Decoder decoder;
+    /* package */ volatile SourceDataLine currentDataLine;
 
-    private volatile SourceDataLine currentDataLine;
+    /* package */ final AtomicReference<PlayerThread> currentPlayerThread = new AtomicReference<PlayerThread>();
 
-    private final AtomicReference<PlayerThread> currentPlayerThread = new AtomicReference<PlayerThread>();
-
-    private AudioFormat getAudioFormatValue() {
-        return new AudioFormat( decoder.getOutputFrequency(),
-                16,
-                decoder.getOutputChannels(),
-                true,
-                false );
-    }
-
-    private DataLine.Info getSourceLineInfo() {
-        //DataLine.Info info = new DataLine.Info(SourceDataLine.class, fmt, 4000);
-        DataLine.Info info = new DataLine.Info( SourceDataLine.class, getAudioFormatValue() );
-        return info;
-    }
+    /* package */ int realInputStreamLength;
 
     /* package */ SourceDataLine getCurrentDataLine() {
         return currentDataLine;
@@ -172,30 +137,11 @@ public final class Player {
         return pumpStream;
     }
 
-    private SourceDataLine createLine() {
-        Line line = null;
-
-        try {
-            line = AudioSystem.getLine( getSourceLineInfo() );
-        } catch( LineUnavailableException ex ) {
-            Logger.getLogger( Player.class.getName() ).log( Level.SEVERE, null, ex );
-        }
-
-        if( line == null || !( line instanceof SourceDataLine ) ) {
-            return null; // TODO: handle problem
-        }
-
-        return (SourceDataLine) line;
+    /* package */ void setPumpStream( SeekablePumpStream pumpStream ) {
+        this.pumpStream = pumpStream;
     }
 
-    private void flush() {
-        final SourceDataLine l = currentDataLine;
-        if( l != null ) {
-            l.drain();
-        }
-    }
-
-    private void populateVolume( SourceDataLine line ) {
+    /* package */ void populateVolume( SourceDataLine line ) {
         if( line != null && line.isControlSupported( FloatControl.Type.MASTER_GAIN ) ) {
             FloatControl c = (FloatControl) line.getControl( FloatControl.Type.MASTER_GAIN );
             float interval = c.getMaximum() - c.getMinimum();
@@ -223,7 +169,7 @@ public final class Player {
         PlayerThread th = currentPlayerThread.get();
 
         if( th == null ) {
-            th = new PlayerThread( Integer.MAX_VALUE );
+            th = new PlayerThread( Integer.MAX_VALUE, this );
             if( currentPlayerThread.compareAndSet( null, th ) )
                 th.start();
         }
@@ -249,7 +195,7 @@ public final class Player {
 
         PlayerThread th = currentPlayerThread.get();
         if( th != null ) {
-            th.requestedState = st;
+            th.setRequestedState( st );
             th.die();
         }
 
@@ -313,292 +259,8 @@ public final class Player {
 //
 //    };
 
-    private InputStream realInputStream;
-
-    private int realInputStreamLength;
+    /* package */ InputStream realInputStream;
 
     private SeekablePumpStream pumpStream;
 
-    private class PlayerThread extends Thread {
-
-        private volatile boolean dieRequested = false;
-
-        private Bitstream bstream;
-
-        private long mspos = 0;
-
-        private int framesPlayed;
-
-        private final int framesToBePlayed;
-
-        private SourceDataLine line;
-
-        private byte[] buff = new byte[8192];
-
-        private InputStream stream;
-
-        private PlayerState requestedState = null;
-
-        public PlayerThread( int framesToBePlayed ) {
-            super( "player-playback-thread" );
-            this.framesToBePlayed = framesToBePlayed;
-        }
-
-        private void createOpenLine() throws LineUnavailableException {
-            line = currentDataLine = createLine();
-            line.open( getAudioFormatValue() );
-            populateVolume( line );
-            //line.addLineListener( lineListener );
-            line.start();
-
-            synchronized( osync ) {
-                state = PlayerState.PLAYING;
-                osync.notifyAll();
-            }
-            new PlayerListenerNotificatorThread( Player.this ).start();
-            new PlayerHelperThread( Player.this ).start();
-        }
-
-        private boolean decodeFrame() throws BitstreamException, DecoderException, LineUnavailableException, InterruptedException {
-            Header h = bstream.readFrame();
-            if( h == null )
-                return false;
-            SampleBuffer sb = (SampleBuffer) decoder.decodeFrame( h, bstream );
-
-            short[] samples = sb.getBuffer();
-            int sz = samples.length << 1;
-            if( sz > buff.length ) {
-                int limit = buff.length + 1024;
-                while( sz > limit )
-                    limit += 1024;
-                buff = new byte[limit];
-            }
-
-            int idx = 0;
-            for( short s : samples ) {  // TODO: change it!!!!!
-                buff[idx++] = (byte) s;
-                buff[idx++] = (byte) ( s >> 8 );
-            }
-
-            if( line == null ) {
-                createOpenLine();    // TODO: бяка!
-                totalPlayTimeMcsec = (long) ( 1000. * h.total_ms( realInputStreamLength ) );
-            }
-
-
-            synchronized( osync ) {
-                while( state == PlayerState.PAUSED_BUFFERING )
-                    osync.wait();
-                if( state == PlayerState.STOPPED )
-                    return false;
-
-                int wasWritten = 0;
-                while( wasWritten < idx && !dieRequested && line.isOpen() ) {
-                    wasWritten += line.write( buff, 0, idx );
-                }
-            }
-
-            framesPlayed++;
-            //written pos in microseconds += (long) (1000.f * h.ms_per_frame());
-
-            bstream.closeFrame();
-            return true;
-        }
-
-        private void openInputStream() throws IOException {
-            String location = sourceLocation;
-            if( location == null )
-                throw new IllegalArgumentException();
-
-            if( location.startsWith( "http://" ) ) {
-                //HttpURLConnection c =
-                URLConnection c = new URL( location ).openConnection();
-                c.connect();
-                realInputStreamLength = c.getContentLength();
-                realInputStream = c.getInputStream();
-            } else {
-                boolean slow = false;
-                if( location.startsWith( "!" ) ) {
-                    // debug case: slow input stream impl
-                    location = location.substring( 1 );
-                    slow = true;
-                }
-                File f = new File( location );
-                if( f.exists() && !f.isDirectory() && f.canRead() )
-                    realInputStream = new FileInputStream( f );
-                else
-                    throw new IllegalArgumentException( "Bad path to file: '" + location + "'" );
-                realInputStreamLength = (int) f.length();
-                if( slow )
-                    realInputStream = new EmulateSlowInputStream( realInputStream, 0.3 );
-            }
-        }
-
-        private void processID3v2() throws IOException {
-            InputStream is = bstream.getRawID3v2();
-            if( is != null ) {
-                System.out.println( "ID3v2 found" );
-                InputStreamReader isr = new InputStreamReader( is );
-                OutputStreamWriter osw = new OutputStreamWriter( System.out );
-                try {
-                    char[] buffer = new char[128];
-                    int wasRead;
-
-                    while( ( wasRead = isr.read( buffer ) ) != -1 ) {
-                        //osw.write( buffer, 0, wasRead );
-                    }
-                    osw.flush();
-                    System.out.println();
-                    System.out.println( "end of ID3" );
-                } finally {
-                    close( isr );
-                    close( is );
-                }
-            }
-        }
-
-        private void resumeFromPause() {
-            synchronized( osync ) {
-                if( state == PlayerState.PAUSED ) {
-                    //currentSeekPositionMcsec = getCurrentPosition(); // TODO: is it actualy required?
-                    //currentPlayTimeMcsec = 0;
-                }
-            }
-        }
-
-        private void skipFrames( long timeMcsec ) throws BitstreamException {
-            long skipped = 0;
-            Header h;
-
-            while( !dieRequested && ( skipped < timeMcsec ) && ( ( h = bstream.readFrame() ) != null ) ) {
-                skipped += (long) ( 1000. * h.ms_per_frame() );
-                bstream.closeFrame();
-            }
-        }
-
-        @Override
-        public void run() {
-            try {
-                if( pumpStream == null ) {
-                    openInputStream();
-                    pumpStream = new SeekablePumpStream( realInputStream );
-
-                    new Thread( "player-stream-pump-thread" ) {
-
-                        @Override
-                        public void run() {
-                            pumpStream.pumpLoop();
-                        }
-
-                    }.start();
-                    new PlayerBufferedResolverThread( Player.this ).start();
-                }
-
-                resumeFromPause();
-                endOfMediaReached = false;
-
-                stream = pumpStream.openStream();
-                bstream = new Bitstream( stream );
-                processID3v2();
-
-                decoder = new Decoder();
-
-                line = currentDataLine = null;
-
-                skipFrames( currentSeekPositionMcsec );
-
-                long lastUpdate = 0;
-
-                while( !dieRequested && framesPlayed < framesToBePlayed ) {
-                    if( !decodeFrame() )
-                        break;
-
-                    currentPlayTimeMcsec = line.getMicrosecondPosition();
-
-                    long now = System.currentTimeMillis();
-                    lastPlayerActivity = now;
-                    if( now - lastUpdate > 250 ) {
-                        synchronized( osync ) {
-                            osync.notifyAll();
-                        }
-                        lastUpdate = now;
-                    }
-
-                }
-
-                if( line != null ) {
-                    line.flush();
-                    line.drain();
-                    currentSeekPositionMcsec += line.getMicrosecondPosition();
-                    currentPlayTimeMcsec = 0;
-                    endOfMediaReached = !dieRequested;
-                    System.out.println( "last play position: " + ( line.getMicrosecondPosition() / 1000 ) + " ms" );
-                }
-
-            } catch( BitstreamException e ) {
-                e.printStackTrace();
-            } catch( InterruptedIOException ignore ) {
-            } catch( IOException e ) {
-                e.printStackTrace();
-            } catch( DecoderException e ) {
-                e.printStackTrace();
-            } catch( LineUnavailableException e ) {
-                e.printStackTrace();
-            } catch( InterruptedException e ) {
-            } finally {
-                try {
-                    if( bstream != null )
-                        bstream.close();
-                } catch( BitstreamException ignore ) {
-                }
-                //close(pumpStream);
-                //close( realInputStream );
-                if( line != null ) {
-                    mspos = line.getMicrosecondPosition() >> 32;
-                    line.stop();
-                    line.close();
-                    currentDataLine = null;
-                }
-
-                if( currentPlayerThread.compareAndSet( this, null ) ) {
-                    // TODO: notify stopped
-                    synchronized( osync ) {
-                        if( requestedState != null ) {
-                            switch( requestedState ) {
-                                case STOPPED:
-                                    currentSeekPositionMcsec = 0;
-                                    state = requestedState;
-                                    break;
-                                case PAUSED:
-                                    state = requestedState;
-                                    break;
-                                default:
-                                    state = PlayerState.STOPPED;
-                                    break;
-                            }
-
-                        } else {
-                            currentSeekPositionMcsec = 0;
-                            currentPlayTimeMcsec = 0;
-                            state = PlayerState.STOPPED;
-                        }
-                        osync.notifyAll();
-                    }
-                }
-
-            }
-        }
-
-        public void die() {
-            dieRequested = true;
-            interrupt();
-        }
-
-        public void setRequestedState( PlayerState requestedState ) {
-            this.requestedState = requestedState;
-        }
-
-    }
-
 }
-
