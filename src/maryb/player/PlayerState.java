2be699c66b5d55e330ca201827b9f00f1c8a6526
cy6erGn0m <cy6erGn0m@localhost>
2009-10-05 02:06:07 +0400

    Docs about player state.

--- src/maryb/player/PlayerState.java
+++ src/maryb/player/PlayerState.java
@@ -15,8 +15,23 @@ package maryb.player;
  * @author cy6erGn0m
  */
 public enum PlayerState {
+    /**
+     * Player stopped
+     */
     STOPPED,
+    /**
+     * Player currently paused. Please note that actuially there is no any 
+     * difference between stop and pause but rusume playback from pause.
+     */
     PAUSED,
+    /**
+     * Playback is running
+     */
     PLAYING,
+    /**
+     * Playback was started but stalled. This may happen when remote resource
+     * connected via slow connection or remote server under high load. Player
+     * restores itself to PLAYING when buffed length will grow.
+     */
     PAUSED_BUFFERING
 }
